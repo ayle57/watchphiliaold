6e9ac74140573461e63f0646216ef9bb4139c492
Aylé <107368715+ayle57@users.noreply.github.com>
2023-11-11 14:29:30 +0100

    Add Kernel

--- public/index.php
+++ public/index.php
@@ -1,7 +1,16 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * IDE: PhpStorm
  * Project: watchphilia
  * User: allistair
  * Year: 2023
  */
+
+use App\Kernel;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$app = new Kernel();
+
+$app->create();
+$app->run();
---
+++ src/Kernel.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * IDE: PhpStorm
+ * Project: watchphilia
+ * User: allistair
+ * Year: 2023
+ */
+
+namespace App;
+
+use App\Helpers\Generators\RouteGenerator;
+use Phroute\Phroute\{Dispatcher, RouteCollector as Router, RouteParser};
+
+class Kernel
+{
+    public Router $router;
+    private Dispatcher $dispatcher;
+    private Mixed $response;
+    public function create()
+    {
+        $router = new Router(new RouteParser());
+        $this->router = $router;
+
+        $routeGenerator = new RouteGenerator($router);
+        require_once dirname(__DIR__) . '/routes/web.php';
+
+        $dispatcher = new Dispatcher($router->getData());
+        $this->dispatcher = $dispatcher;
+
+        $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], \PHP_URL_PATH);
+        $this->response = $response;
+    }
+
+    public function run()
+    {
+        $response = $this->response;
+        echo $response;
+    }
+}
